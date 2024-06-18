3a3fa53a136691d13c77f0baad466cdcd6dbadf3
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-06-18 12:52:03 -0300

    Refactor intro page into components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IntroPageComponent } from './intro-page/intro-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: IntroPageComponent}
+];
